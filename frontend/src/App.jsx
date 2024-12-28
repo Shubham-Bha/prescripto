1f552841214df1e696f3539914a5a9251bad392c
Shubham-Bha <135313817+Shubham-Bha@users.noreply.github.com>
2024-12-29 00:34:20 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 //import React from 'react';
 import {Route, Routes} from  'react-router-dom';
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Doctors from './pages/doctors';
 import Login from './pages/login';
 import About from './pages/About';
