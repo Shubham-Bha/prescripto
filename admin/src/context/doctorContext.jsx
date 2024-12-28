7f85e923e7930e84e01296d1aaf259714894b316
Shubham-Bha <135313817+Shubham-Bha@users.noreply.github.com>
2024-12-29 00:11:38 +0530

    Update doctorContext.jsx

--- admin/src/context/doctorContext.jsx
+++ admin/src/context/doctorContext.jsx
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://prescripto-backend-ek9m.onrender.com"
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   );
